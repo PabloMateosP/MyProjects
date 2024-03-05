213a5fc2097683bd0e48c3c1d9169ee46443b4d3
unknown <pablomp1301@gmail.com>
2024-03-05 13:26:16 +0100

    PabloMP - Subida Añadir Función Pantalla Carga - 05/03/24

--- TFG/src/assets/scripts/index.js
+++ TFG/src/assets/scripts/index.js
@@ -1,22 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var floatingMenu = document.querySelector(".floating-menu");
-    var floatingFooter = document.querySelector(".floating-footer");
+// document.addEventListener("DOMContentLoaded", function() {
+//     var floatingMenu = document.querySelector(".floating-menu");
+//     var floatingFooter = document.querySelector(".floating-footer");
 
-    floatingMenu.style.opacity = "0";
-    floatingFooter.style.opacity = "0";
+//     floatingMenu.style.opacity = "0";
+//     floatingFooter.style.opacity = "0";
 
-    var contadorSegundos = 2;
+//     var contadorSegundos = 2;
 
-    function mostrarDespuesDeSegundos() {
-        var fadeInInterval = setInterval(function() {
-            if (parseFloat(floatingMenu.style.opacity) < 1) {
-                floatingMenu.style.opacity = (parseFloat(floatingMenu.style.opacity) + 0.2).toString();
-                floatingFooter.style.opacity = (parseFloat(floatingFooter.style.opacity) + 0.2).toString();
-            } else {
-                clearInterval(fadeInInterval);
-            }
-        }, 100);
-    }
+//     function mostrarDespuesDeSegundos() {
+//         var fadeInInterval = setInterval(function() {
+//             if (parseFloat(floatingMenu.style.opacity) < 1) {
+//                 floatingMenu.style.opacity = (parseFloat(floatingMenu.style.opacity) + 0.2).toString();
+//                 floatingFooter.style.opacity = (parseFloat(floatingFooter.style.opacity) + 0.2).toString();
+//             } else {
+//                 clearInterval(fadeInInterval);
+//             }
+//         }, 100);
+//     }
 
-    setTimeout(mostrarDespuesDeSegundos, contadorSegundos * 1000);
-});
+//     setTimeout(mostrarDespuesDeSegundos, contadorSegundos * 1000);
+// });
+
+// FUNCIÓN PARA OCULTAR DURANTE DOS SEGUNDO EL HEADER Y EL FOOTER
+// -------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------
+// FUNCIÓN PARA MOSTRAR PÁGINA DE CARGA 
+window.onload = function() {
+
+    // Tiempo de espera en milisegundos
+    var delay = 2000; 
+
+    setTimeout(function() {
+        // Ocultar la página de carga
+        document.getElementById('loading').style.opacity = '0';
+
+        // Mostrar la página principal
+        document.getElementById('home').style.display = 'block';
+    }, delay);
+};
